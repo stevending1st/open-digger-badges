6c09cffef1cb9e7aa79a1fc129ec9b792bae9562
stevending1st <stevending1st@163.com>
2023-03-07 00:25:43 +0800

    feat: fetch data function;

---
+++ src/common/fetchData.ts
@@ -0,0 +1,24 @@
+export interface FetchMetricsDataInputType {
+  owner: string,
+  repo?: string,
+  metric: string,
+}
+
+export type FetchBatchMetricsDataInputType = Omit<FetchMetricsDataInputType, 'metric'> & {
+  metrics: string[]
+}
+
+export const fetchMetricsData = async ({ owner, repo, metric }: FetchMetricsDataInputType) => {
+  const json = await fetch(`https://oss.x-lab.info/open_digger/github/${owner}/${!repo ? '' : repo + '/'}${metric}.json`);
+  const result = await json.json();
+  return result
+}
+
+export const fetchBatchMetricsData = async ({ owner, repo, metrics }: FetchBatchMetricsDataInputType) => {
+  let resultBatchData = {};
+  for (let metric of metrics) {
+    const result = await fetchMetricsData({ owner, repo, metric})
+    resultBatchData = {...resultBatchData, [metric]: result}
+  }
+  return resultBatchData;
+}
--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,11 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { checkQuery } from '@/common/check';
+import { fetchBatchMetricsData } from '@/common/fetchData';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  checkQuery(req.query)
-  res.status(200).send(req.query);
+  try {
+    const { owner,repo, metrics } = checkQuery(req.query);
+    const re = await fetchBatchMetricsData({owner,repo, metrics})
+    res.status(200).json(re);
+  } catch (err: any) {
+    res.status(200).send("❌ ERROR:"+ err.message);
+  }
 }

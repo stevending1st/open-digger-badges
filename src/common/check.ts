f31b107d65f3ebeebeccf410e7290c57a72a69a4
stevending1st <stevending1st@163.com>
2023-03-06 19:36:28 +0800

    feat: add scopeCheck function;

--- src/common/check.ts
+++ src/common/check.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest } from "next";
-import { ScopeType } from "./scope";
+import { metricScope, ScopeType } from "./scope";
 
 export const checkOwnerAndRepo = (query: NextApiRequest['query']) => {
   const { owner, repo } = query;
@@ -8,10 +8,25 @@ export const checkOwnerAndRepo = (query: NextApiRequest['query']) => {
   return ScopeType.USER;
 }
 
+export const scopeCheck = (scope: ScopeType, query: NextApiRequest['query']) => {
+  const { metric } = query;
+  if(!metric) throw Error('without metric')
+  const metricArr = typeof metric === 'string' ? metric.split(",") : metric;
+  const metricNotInScope = metricArr.reduce<string[]>((pre, cur) => {
+    const currentScope = metricScope[cur.toLocaleLowerCase() as keyof typeof metricScope]
+    if(!currentScope) return [...pre, cur];
+    if(!currentScope.includes(scope)) return [...pre, cur];
+    return pre;
+  } , [])
+  if(metricNotInScope.length > 0) throw Error(`${metricNotInScope.join(",")} isn't in scope`)
+  return metricArr;
+}
+
 export const checkQuery = (query: NextApiRequest['query']) => {
   try {
     const scope = checkOwnerAndRepo(query);
-    console.log("✅ scope:", scope)
+    const metricArr = scopeCheck(scope, query);
+    console.log("✅ metricArr:", metricArr.join(","))
   } catch(err: any) {
     console.log("❌ ERROR:", err.message);
   }

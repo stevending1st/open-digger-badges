ef363a0213d55253a12f4f8665fabe793f4a7d81
stevending1st <stevending1st@163.com>
2023-03-06 23:25:16 +0800

    feat: change check functions

--- src/common/check.ts
+++ src/common/check.ts
@@ -4,32 +4,30 @@ import { metricScope, ScopeType } from "./scope";
 export const checkOwnerAndRepo = (query: NextApiRequest['query']) => {
   const { owner, repo } = query;
   if (!owner) throw Error('without owner');
-  if (repo) return ScopeType.REPO;
-  return ScopeType.USER;
+  if (Array.isArray(owner)) throw Error("owner cannot be an array type");
+  if (Array.isArray(repo)) throw Error("owner cannot be an array type");
+  if (repo) return { owner, repo, scope: ScopeType.REPO };
+  return { owner, scope: ScopeType.USER };
 }
 
 export const scopeCheck = (scope: ScopeType, query: NextApiRequest['query']) => {
   const { metric } = query;
-  if(!metric) throw Error('without metric')
+  if (!metric) throw Error('without metric')
   const metricArr = typeof metric === 'string' ? metric.split(",") : metric;
   const metricNotInScope = metricArr.reduce<string[]>((pre, cur) => {
     const currentScope = metricScope[cur.toLocaleLowerCase() as keyof typeof metricScope]
-    if(!currentScope) return [...pre, cur];
-    if(!currentScope.includes(scope)) return [...pre, cur];
+    if (!currentScope) return [...pre, cur];
+    if (!currentScope.includes(scope)) return [...pre, cur];
     return pre;
-  } , [])
-  if(metricNotInScope.length > 0) throw Error(`${metricNotInScope.join(",")} isn't in scope`)
+  }, [])
+  if (metricNotInScope.length > 0) throw Error(`${metricNotInScope.join(",")} isn't in scope`)
   return metricArr;
 }
 
 export const checkQuery = (query: NextApiRequest['query']) => {
-  try {
-    const scope = checkOwnerAndRepo(query);
-    const metricArr = scopeCheck(scope, query);
-    console.log("✅ metricArr:", metricArr.join(","))
-  } catch(err: any) {
-    console.log("❌ ERROR:", err.message);
-  }
+  const { owner, repo, scope } = checkOwnerAndRepo(query);
+  const metricArr = scopeCheck(scope, query);
+  return { owner, repo, metrics: metricArr }
 }
 
 

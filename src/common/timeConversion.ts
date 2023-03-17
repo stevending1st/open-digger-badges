fc11ba9adb4f6b341c6f40020015358abf09d603
stevending1st <stevending1st@163.com>
2023-03-18 01:15:32 +0800

    fix: when monthNum = 0, show the munber of month

--- src/common/timeConversion.ts
+++ src/common/timeConversion.ts
@@ -1,4 +1,5 @@
 export const metricsRangeCalculation = (monthNum: number) =>{
+  if(monthNum === 0) return "0 month"
   if(monthNum % 12 === 0) {
     const yearNum = monthNum / 12;
     return `${yearNum} year${yearNum > 1 ?  "s" : ''}`

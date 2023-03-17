ca7af4c0fafd9ddc5cdf65c19c29c7fb16a1889f
stevending1st <stevending1st@163.com>
2023-03-18 00:32:54 +0800

    feat: add svg's style

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -11,12 +11,13 @@ export default async function handler(
 ) {
   try {
     res.setHeader("Content-Type", "image/svg+xml");
-    const { owner,repo, metrics, month } = checkQuery(req.query);
+    const { owner,repo, metrics, month, badgeStyle } = checkQuery(req.query);
     const batchMetricsData = await fetchBatchMetricsData({owner,repo, metrics})
     const re = indexFilter(batchMetricsData[metrics[0]], month)
     const svg = makeBadge({
       label: metrics[0],
       message: re.toString(),
+      style: badgeStyle
     })
     res.status(200).send(svg);
   } catch (err: any) {

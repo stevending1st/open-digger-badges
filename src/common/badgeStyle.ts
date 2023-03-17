2c538d0291df9eff5c4b5056b5d29e2921040339
stevending1st <stevending1st@163.com>
2023-03-18 00:28:35 +0800

    feat: add BadgeStyleType

---
+++ src/common/badgeStyle.ts
@@ -0,0 +1,9 @@
+export enum BadgeStyleType {
+    PLASTIC = 'plastic',
+    FLAT = 'flat',
+    FLAT_SQUARE = 'flat-square',
+    FOR_THE_BADGE = 'for-the-badge',
+    SOCIAL = 'social',
+}
+
+export const allBadgeStyleTypeArr = [BadgeStyleType.PLASTIC, BadgeStyleType.FLAT, BadgeStyleType.FLAT_SQUARE, BadgeStyleType.FOR_THE_BADGE, BadgeStyleType.SOCIAL]

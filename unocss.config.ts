f09985bdc4780b6d3617185cd1704a892b3f7fd1
stevending1st <stevending1st@163.com>
2023-03-18 23:55:29 +0800

    feat: add unocss.config.ts

---
+++ unocss.config.ts
@@ -0,0 +1,8 @@
+// unocss.config.ts
+import { defineConfig, presetAttributify, presetIcons, presetUno, presetWebFonts } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+  ],
+})

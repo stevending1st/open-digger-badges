6774b1419d7dae0ab8953e49efd3aa7f1d9bb06f
stevending1st <stevending1st@163.com>
2023-03-18 23:56:36 +0800

    add unocss to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+
 import '@/styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {

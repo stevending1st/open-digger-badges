f877628159bc74b00b4489bae23f6ff94e5e70dc
stevending1st <stevending1st@163.com>
2023-03-20 16:30:33 +0800

    refactor: refactor OBDInput component

--- src/components/OBDInput.tsx
+++ src/components/OBDInput.tsx
@@ -1,14 +1,13 @@
 import classNames from "classnames";
+import { forwardRef } from "react";
 
-export interface ODBInputProps {
-    placeholder?: string,
-    className?: string,
-    name?: string,
-    id?: string,
-    size?: HTMLInputElement["size"],
-    value?: HTMLInputElement["value"],
-    onChange?: (value: any) => any,
+export interface ODBInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    id?: string;
 }
 
-export const ODBInput = ({placeholder, className, name, id, size, value, onChange}: ODBInputProps) =>
-    <input className={classNames("border-t-none border-l-none border-r-none outline-none border-b-1 mx-1", className)} {...{placeholder, name, id, value, size}} onChange={onChange}/>
+export const ODBInput = forwardRef<HTMLInputElement, ODBInputProps>((props, ref) => {
+    const { id, className, ...rest } = props;
+    return (<input {...rest} className={classNames("border-t-none border-l-none border-r-none outline-none border-b-1 mx-1", className)} ref={ref} />)
+})
+
+ODBInput.displayName = 'ODBInput';
